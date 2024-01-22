19a0d38fa033b80c03dd223db9b303ff4005978b
SyLi_4aaf <shaoyan.li@uzh.ch>
2024-01-22 16:40:04 +0100

    chore: change mongodb server

--- api/indexInbound.js
+++ api/indexInbound.js
@@ -5,10 +5,10 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-const uri = 'mongodb+srv://GraceLTQ:Apple_030711@outboundemailcluster.xapxelz.mongodb.net/';
+const uri = "mongodb+srv://Admin:NodesAdvisors2024@dev.jq8me.mongodb.net/";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const dbName = 'EpsilonEmailDB';
+const dbName = 'dev';
 const InboundCollectionName = 'InboundEmails';
 
 async function connectToMongoDB() {
@@ -40,6 +40,84 @@ app.get('/', async (req, res) => {
     res.status(500).send('Error fetching data');
   }
 });
+
+
+app.get('/employees', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('dev');
+    const collection = database.collection('Employees');
+    const employees = await collection.find({}).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/fundcard', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('dev');
+    const collection = database.collection('FundCard');
+    const employees = await collection.find({}).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/inboundemails', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('dev');
+    const collection = database.collection('InboundEmails');
+    const employees = await collection.find({}).limit(500).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/kpisInfo', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('dev');
+    const collection = database.collection('KPIsInfo');
+    const employees = await collection.find({}).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/outboundEmails', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('dev');
+    const collection = database.collection('OutboundEmails');
+    const employees = await collection.find({}).limit(500).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  } finally {
+    await client.close();
+  }
+});
+
+
 
 app.listen(5001, async () => {
   console.log('API is working!');
